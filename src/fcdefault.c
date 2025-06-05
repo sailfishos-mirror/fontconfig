sailfishos-mirror/fontconfig

commit 90a84c477c4006b5539973a5487f28b3ed09711d

Drop FcDefaultFini() from FcFini() to fix memory leaks
All relevant variables has been moved into FcConfig instance and
to deal with this change, new APIs, FcConfigGetDefaultLangs(),
FcConfigSetDefaultSubstitute() has been added. FcGetDefaultLangs()
and FcDefaultSubstitute() still works and keep available for backward
compatibility though, they are referring the default FcConfig instance
from FcConfigGetCurrent() from now on.

If a code creates multiple FcConfig instances, this change may affects.

Changelog: changed

src/fcdefault.c
@@ -48,11 +48,16 @@ static const struct {
 FcStrSet *default_langs;
 
 FcStrSet *
-FcGetDefaultLangs (void)
+FcConfigGetDefaultLangs (FcConfig *config)
 {
     FcStrSet *result;
+
+    if (!config) {
+	config = FcConfigGetCurrent();
+    }
+    FcConfigReference (config);
 retry:
-    result = (FcStrSet *)fc_atomic_ptr_get (&default_langs);
+    result = (FcStrSet *)fc_atomic_ptr_get (&config->default_langs);
     if (!result) {
 	char *langs;
 
@@ -80,45 +85,65 @@ retry:
 	    FcStrSetAdd (result, (const FcChar8 *)"en");
 
 	FcRefSetConst (&result->ref);
-	if (!fc_atomic_ptr_cmpexch (&default_langs, NULL, result)) {
+	if (!fc_atomic_ptr_cmpexch (&config->default_langs, NULL, result)) {
 	    FcRefInit (&result->ref, 1);
 	    FcStrSetDestroy (result);
 	    goto retry;
 	}
     }
+    FcConfigDestroy (config);
 
     return result;
 }
 
-static FcChar8 *default_lang; /* MT-safe */
+FcStrSet *
+FcGetDefaultLangs (void)
+{
+    return FcConfigGetDefaultLangs (NULL);
+}
 
 FcChar8 *
-FcGetDefaultLang (void)
+FcConfigGetDefaultLang (FcConfig *config)
 {
     FcChar8 *lang;
+
+    if (!config) {
+	config = FcConfigGetCurrent();
+    }
+    FcConfigReference (config);
 retry:
-    lang = fc_atomic_ptr_get (&default_lang);
+    lang = fc_atomic_ptr_get (&config->default_lang);
     if (!lang) {
-	FcStrSet *langs = FcGetDefaultLangs();
+	FcStrSet *langs = FcConfigGetDefaultLangs (config);
 	lang = FcStrdup (langs->strs[0]);
 
-	if (!fc_atomic_ptr_cmpexch (&default_lang, NULL, lang)) {
+	if (!fc_atomic_ptr_cmpexch (&config->default_lang, NULL, lang)) {
 	    free (lang);
 	    goto retry;
 	}
     }
+    FcConfigDestroy (config);
 
     return lang;
 }
 
-static FcChar8 *default_prgname;
+FcChar8 *
+FcGetDefaultLang (void)
+{
+    return FcConfigGetDefaultLang (NULL);
+}
 
 FcChar8 *
-FcGetPrgname (void)
+FcConfigGetPrgname (FcConfig *config)
 {
     FcChar8 *prgname;
+
+    if (!config) {
+	config = FcConfigGetCurrent();
+    }
+    FcConfigReference (config);
 retry:
-    prgname = fc_atomic_ptr_get (&default_prgname);
+    prgname = fc_atomic_ptr_get (&config->prgname);
     if (!prgname) {
 #ifdef _WIN32
 	char buf[MAX_PATH + 1];
@@ -197,26 +222,38 @@ retry:
 	    free (p);
 #endif
 
-	if (!fc_atomic_ptr_cmpexch (&default_prgname, NULL, prgname)) {
+	if (!fc_atomic_ptr_cmpexch (&config->prgname, NULL, prgname)) {
 	    free (prgname);
 	    goto retry;
 	}
     }
 
-    if (prgname && !prgname[0])
-	return NULL;
+    if (prgname && !prgname[0]) {
+	free (prgname);
+	prgname = NULL;
+    }
+    FcConfigDestroy (config);
 
     return prgname;
 }
 
-static FcChar8 *default_desktop_name;
+FcChar8 *
+FcGetPrgname (void)
+{
+    return FcConfigGetPrgname (NULL);
+}
 
 FcChar8 *
-FcGetDesktopName (void)
+FcConfigGetDesktopName (FcConfig *config)
 {
     FcChar8 *desktop_name;
+
+    if (!config) {
+	config = FcConfigGetCurrent();
+    }
+    FcConfigReference (config);
 retry:
-    desktop_name = fc_atomic_ptr_get (&default_desktop_name);
+    desktop_name = fc_atomic_ptr_get (&config->desktop_name);
     if (!desktop_name) {
 	char *s = getenv ("XDG_CURRENT_DESKTOP");
 
@@ -230,49 +267,28 @@ retry:
 	    return NULL;
 	}
 
-	if (!fc_atomic_ptr_cmpexch (&default_desktop_name, NULL, desktop_name)) {
+	if (!fc_atomic_ptr_cmpexch (&config->desktop_name, NULL, desktop_name)) {
 	    free (desktop_name);
 	    goto retry;
 	}
     }
-    if (desktop_name && !desktop_name[0])
-	return NULL;
+    if (desktop_name && !desktop_name[0]) {
+	desktop_name = NULL;
+    }
+    FcConfigDestroy (config);
 
     return desktop_name;
 }
 
-void
-FcDefaultFini (void)
+FcChar8 *
+FcGetDesktopName (void)
 {
-    FcChar8  *lang;
-    FcStrSet *langs;
-    FcChar8  *prgname;
-    FcChar8  *desktop;
-
-    lang = fc_atomic_ptr_get (&default_lang);
-    if (lang && fc_atomic_ptr_cmpexch (&default_lang, lang, NULL)) {
-	free (lang);
-    }
-
-    langs = fc_atomic_ptr_get (&default_langs);
-    if (langs && fc_atomic_ptr_cmpexch (&default_langs, langs, NULL)) {
-	FcRefInit (&langs->ref, 1);
-	FcStrSetDestroy (langs);
-    }
-
-    prgname = fc_atomic_ptr_get (&default_prgname);
-    if (prgname && fc_atomic_ptr_cmpexch (&default_prgname, prgname, NULL)) {
-	free (prgname);
-    }
-
-    desktop = fc_atomic_ptr_get (&default_desktop_name);
-    if (desktop && fc_atomic_ptr_cmpexch (&default_desktop_name, desktop, NULL)) {
-	free (desktop);
-    }
+    return FcConfigGetDesktopName (NULL);
 }
 
 void
-FcDefaultSubstitute (FcPattern *pattern)
+FcConfigSetDefaultSubstitute (FcConfig  *config,
+                              FcPattern *pattern)
 {
     FcPatternIter iter;
     FcValue       v, namelang, v2;
@@ -331,7 +347,7 @@ FcDefaultSubstitute (FcPattern *pattern)
 	FcPatternObjectAddInteger (pattern, FC_HINT_STYLE_OBJECT, FC_HINT_FULL);
 
     if (!FcPatternFindObjectIter (pattern, &iter, FC_NAMELANG_OBJECT))
-	FcPatternObjectAddString (pattern, FC_NAMELANG_OBJECT, FcGetDefaultLang());
+	FcPatternObjectAddString (pattern, FC_NAMELANG_OBJECT, FcConfigGetDefaultLang (config));
 
     /* shouldn't be failed. */
     FcPatternObjectGet (pattern, FC_NAMELANG_OBJECT, 0, &namelang);
@@ -362,19 +378,25 @@ FcDefaultSubstitute (FcPattern *pattern)
     }
 
     if (FcPatternObjectGet (pattern, FC_PRGNAME_OBJECT, 0, &v) == FcResultNoMatch) {
-	FcChar8 *prgname = FcGetPrgname();
+	FcChar8 *prgname = FcConfigGetPrgname (config);
 	if (prgname)
 	    FcPatternObjectAddString (pattern, FC_PRGNAME_OBJECT, prgname);
     }
 
     if (FcPatternObjectGet (pattern, FC_DESKTOP_NAME_OBJECT, 0, &v) == FcResultNoMatch) {
-	FcChar8 *desktop = FcGetDesktopName();
+	FcChar8 *desktop = FcConfigGetDesktopName (config);
 	if (desktop)
 	    FcPatternObjectAddString (pattern, FC_DESKTOP_NAME_OBJECT, desktop);
     }
 
     if (!FcPatternFindObjectIter (pattern, &iter, FC_ORDER_OBJECT))
 	FcPatternObjectAddInteger (pattern, FC_ORDER_OBJECT, 0);
+}
+
+void
+FcDefaultSubstitute (FcPattern *pattern)
+{
+    FcConfigSetDefaultSubstitute (NULL, pattern);
 }
 #define __fcdefault__
 #include "fcaliastail.h"

doc/fclangset.fncs
@@ -164,7 +164,20 @@ has no matching language, this function returns FcLangDifferentLang.
 Returns a string set of the default languages according to the environment variables on the system.
 This function looks for them in order of FC_LANG, LC_ALL, LC_CTYPE and LANG then.
 If there are no valid values in those environment variables, "en" will be set as fallback.
+  </para><para>
+This function has been replaced by <function>FcConfigGetDefaultLangs</function> and will be deprecated.
 @SINCE@         2.9.91
+@@
+
+@RET@           FcStrSet *
+@FUNC@          FcConfigGetDefaultLangs
+@TYPE1@         FcConfig *                      @ARG1@          config
+@PURPOSE@       Get the default languages list
+@DESC@
+Returns a string set of the default languages according to the environment variables on the system.
+This function looks for them in order of FC_LANG, LC_ALL, LC_CTYPE and LANG then.
+If there are no valid values in those environment variables, "en" will be set as fallback.
+@SINCE@         2.17.0
 @@
 
 @RET@           FcStrSet *

doc/fcpattern.fncs
@@ -521,6 +521,31 @@ Patterns without a specified pixel size are given one computed from any
 specified point size (default 12), dpi (default 75) and scale (default 1).
 </para></listitem>
 </itemizedlist>
+  </para><para>
+This function has been replaced by <function>FcConfigSetDefaultSubstitute</function>
+and will be deprecated.
+@@
+
+@RET@           void
+@FUNC@          FcConfigSetDefaultSubstitute
+@TYPE1@         FcConfig *                      @ARG1@          config
+@TYPE2@         FcPattern *                     @ARG2@          pattern
+@PURPOSE@       Perform default substitutions in a pattern
+@DESC@
+Supplies default values for underspecified font patterns:
+<itemizedlist>
+<listitem><para>
+Patterns without a specified style or weight are set to Medium
+</para></listitem>
+<listitem><para>
+Patterns without a specified style or slant are set to Roman
+</para></listitem>
+<listitem><para>
+Patterns without a specified pixel size are given one computed from any
+specified point size (default 12), dpi (default 75) and scale (default 1).
+</para></listitem>
+</itemizedlist>
+@SINCE@         2.17.0
 @@
 
 @RET@           FcPattern *

fc-match/fc-match.c
@@ -183,7 +183,7 @@ main (int argc, char **argv)
 	return 1;
 
     FcConfigSubstitute (0, pat, FcMatchPattern);
-    FcDefaultSubstitute (pat);
+    FcConfigSetDefaultSubstitute (0, pat);
 
     fs = FcFontSetCreate();
 

fc-pattern/fc-pattern.c
@@ -168,7 +168,7 @@ main (int argc, char **argv)
     if (do_config)
 	FcConfigSubstitute (0, pat, FcMatchPattern);
     if (do_default)
-	FcDefaultSubstitute (pat);
+	FcConfigSetDefaultSubstitute (0, pat);
 
     if (os) {
 	FcPattern *new;

fontconfig/fontconfig.h
@@ -609,7 +609,14 @@ FcFontSetPrint (const FcFontSet *s);
 
 /* fcdefault.c */
 FcPublic FcStrSet *
+FcConfigGetDefaultLangs (FcConfig *config);
+
+FcPublic FcStrSet *
 FcGetDefaultLangs (void);
+
+FcPublic void
+FcConfigSetDefaultSubstitute (FcConfig  *config,
+                              FcPattern *pattern);
 
 FcPublic void
 FcDefaultSubstitute (FcPattern *pattern);

src/fccfg.c
@@ -202,6 +202,10 @@ FcConfigCreate (void)
     config->filter_func = NULL;
     config->filter_data = NULL;
     config->destroy_data_func = NULL;
+    config->default_lang = NULL;
+    config->default_langs = NULL;
+    config->prgname = NULL;
+    config->desktop_name = NULL;
 
     config->prefer_app_fonts = FcFalse;
 
@@ -382,6 +386,15 @@ FcConfigDestroy (FcConfig *config)
 
 	if (config->filter_data && config->destroy_data_func)
 	    config->destroy_data_func (config->filter_data);
+
+	if (config->default_lang)
+	    FcStrFree (config->default_lang);
+	if (config->default_langs)
+	    FcStrSetDestroy (config->default_langs);
+	if (config->prgname)
+	    FcStrFree (config->prgname);
+	if (config->desktop_name)
+	    FcStrFree (config->desktop_name);
 
 	free (config);
     }
@@ -1935,7 +1948,7 @@ FcConfigSubstituteWithPat (FcConfig   *config,
 
     s = config->subst[kind];
     if (kind == FcMatchPattern) {
-	strs = FcGetDefaultLangs();
+	strs = FcConfigGetDefaultLangs (config);
 	if (strs) {
 	    FcStrList *l = FcStrListCreate (strs);
 	    FcChar8   *lang;
@@ -1982,7 +1995,7 @@ FcConfigSubstituteWithPat (FcConfig   *config,
 	    FcLangSetDestroy (lsund);
 	}
 	if (FcPatternObjectGet (p, FC_PRGNAME_OBJECT, 0, &v) == FcResultNoMatch) {
-	    FcChar8 *prgname = FcGetPrgname();
+	    FcChar8 *prgname = FcConfigGetPrgname (config);
 	    if (prgname)
 		FcPatternObjectAddString (p, FC_PRGNAME_OBJECT, prgname);
 	}

src/fcinit.c
@@ -215,7 +215,6 @@ FcFini (void)
 {
     FcConfigFini();
     FcConfigPathFini();
-    FcDefaultFini();
     FcObjectFini();
     FcCacheFini();
 }

commit 3e0cee12f133609ed3dcc41a1f3c180ac725f93a

Fix use-after-free in FcConfigGetPrgname
This fixes a regression introduced in 90a84c47.

Changelog: fixed

src/fcdefault.c
@@ -229,7 +229,6 @@ retry:
     }
 
     if (prgname && !prgname[0]) {
-	free (prgname);
 	prgname = NULL;
     }
     FcConfigDestroy (config);