sailfishos-mirror/fontconfig

commit e42188283f0ee1fb23089f16dbf95e0f3bcbacaf

do not mix up a slash and a backslash in file object on Win32
All path delimitors in file object will be / with this change.
Some config files may need to be updated.

Changelog: changed

src/fcfreetype.c
@@ -1209,6 +1209,7 @@ FcFreeTypeQueryFaceInternal (const FT_Face   face,
 
     FcBool   symbol = FcFalse;
     FT_Error ftresult;
+    FcChar8 *canon_file = NULL;
 
     FcInitDebug(); /* We might be called with no initizalization whatsoever. */
 
@@ -1713,7 +1714,8 @@ FcFreeTypeQueryFaceInternal (const FT_Face   face,
 	    goto bail1;
     }
 
-    if (file && *file && !FcPatternObjectAddString (pat, FC_FILE_OBJECT, file))
+    canon_file = FcStrCanonFilename(file);
+    if (canon_file && *canon_file && !FcPatternObjectAddString (pat, FC_FILE_OBJECT, canon_file))
 	goto bail1;
 
     if (!FcPatternObjectAddInteger (pat, FC_INDEX_OBJECT, id))
@@ -2106,6 +2108,8 @@ bail1:
 	free (name_mapping);
     if (foundry_)
 	free (foundry_);
+    if (canon_file)
+	free (canon_file);
 bail0:
     return NULL;
 }

commit add2f6c6eb23107ad5dc59948a9b694cd5c6a096

Workaround :-prefixed filename used in Qt
Qt6 seems using :-prefixing to take care of some special case and
the change of e4218828 broke this behavior.
So do not call FcStrCanonFilename in this case to work this issue
around.

Fixes https://gitlab.freedesktop.org/fontconfig/fontconfig/-/work_items/528

Changelog: fixed

src/fcfreetype.c
@@ -1714,7 +1714,13 @@ FcFreeTypeQueryFaceInternal (const FT_Face   face,
 	    goto bail1;
     }
 
-    canon_file = FcStrCanonFilename(file);
+    /* Qt6 seems using :-prefixing to take care of some special case.
+     * Do not call FcStrCanonFilename not to break that
+     */
+    if (file[0] != ':')
+	canon_file = FcStrCanonFilename (file);
+    else
+	canon_file = file;
     if (canon_file && *canon_file && !FcPatternObjectAddString (pat, FC_FILE_OBJECT, canon_file))
 	goto bail1;
 
@@ -2114,7 +2120,7 @@ FcFreeTypeQueryFaceInternal (const FT_Face   face,
     FcCharSetDestroy (cs);
     if (foundry_)
 	free (foundry_);
-    if (canon_file)
+    if (canon_file && canon_file != file)
 	free (canon_file);
 
     if (mmvar) {